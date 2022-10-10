d8c3c1b5e4a634e074ebf861de4fa71c3c076aee
sowmya <k.l.sowmya219@gmail.com>
2022-10-10 13:40:45 +0200

    first commit

---
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,30 @@
+import { Component , OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from "../posts.service";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+
+})
+export class PostListComponent implements OnInit{
+  posts: Post[] = [];
+  private postsSub!: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+      this.posts =this.postsService.getPosts();
+      this.postsSub = this.postsService.getPostUpdateList()
+      .subscribe((posts: Post[]) => {
+          this.posts = posts;
+      });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
+}
